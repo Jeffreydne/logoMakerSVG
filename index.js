54a2a098313e62f3ef7b3d3a4598d566177168b9
Jeffreydne <Jeffreydne@gmail.com>
2024-01-12 08:40:03 -0800

    make individual shape classes work

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const {writeFile} = require('fs/promises');
-const Shapes = require('./lib/shapes.js');
+const {Circle, Square, Triangle} = require('./lib/shapes.js');
+// const {Triangle, Circle, Square} = require('./lib/shapes.js');
 
 inquirer
 .prompt([
@@ -27,25 +28,31 @@ inquirer
     },
 ])
 .then((response) => {
-    let shapeString;
-    let textString = response.textChosen;
-    let textColorString = response.textColorChosen;
-    let logoColor = response.logoColorChosen;
+    // let shapeString;
+    // let textString = response.textChosen;
+    // let textColorString = response.textColorChosen;
+    // let logoColor = response.logoColorChosen;
     switch(response.shapeChosen) {
         case "triangle":
-            shapeString = 'polygon points="50,170 150,10 250,170"';
+            let triangleToRender = new Triangle(response.logoColorChosen, response.textChosen, response.textColorChosen);
+            return writeFile(`./examples/${response.shapeChosen}Logo.svg`, triangleToRender.render());
+            // shapeString = 'polygon points="50,170 150,10 250,170"';
             break;
         case "square":
-            shapeString = 'rect x="70" y="20" width="160" height ="160"';
+            // shapeString = 'rect x="70" y="20" width="160" height ="160"';
+            let squareToRender = new Square(response.logoColorChosen, response.textChosen, response.textColorChosen);
+            return writeFile(`./examples/${response.shapeChosen}Logo.svg`, squareToRender.render());
             break;
         case "circle":
-            shapeString = 'circle cx="150" cy="100" r="80"';
+            // shapeString = 'circle cx="150" cy="100" r="80"';
+            let circleToRender = new Circle(response.logoColorChosen, response.textChosen, response.textColorChosen);
+            return writeFile(`./examples/${response.shapeChosen}Logo.svg`, circleToRender.render());
             break;
         default:
             console.log("You didn't pick a shape.");
             break;
     }
-    let logoToRender = new Shapes(shapeString, logoColor, textString, textColorString);
-    let renderSVG = logoToRender.render();
-    return writeFile(`./examples/${response.shapeChosen}Logo.svg`, renderSVG)
+    // let logoToRender = new Shapes(shapeString, logoColor, textString, textColorString);
+    // let renderSVG = logoToRender.render();
+    // return writeFile(`./examples/${response.shapeChosen}Logo.svg`, renderSVG)
 });
